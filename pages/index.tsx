96a4f1590053fe78916278ececce4b96ce41925b
KikoPT1234 <carricokiko2006@gmail.com>
2021-03-12 14:34:51 +0100

    button prototyping

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,14 @@ const Home = () => {
 			<div className="banner h-5/6 w-full relative">
 				<div className="banner-img" />
 				<div className="text-white font-extrabold max-w-none absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-11/12 text-center">
-					<h1 className="text-3xl sm:text-5xl xl:text-7xl mt-4">
-						Free, community-first
+					<h1 className="text-3xl sm:text-5xl xl:text-7xl mt-4 text-primary-50">
+						Free, high quality
 					</h1>
-					<h1 className="text-3xl sm:text-5xl xl:text-7xl mb-4 text-indigo-200">
+					<h1 className="text-3xl sm:text-5xl xl:text-7xl mb-4 text-primary-50">
 						Minecraft server hosting
 					</h1>
-					<button className="mt-4 btn btn-big btn-filled btn--indigo">
-						Get Started
+					<button className="mt-4 btn btn-big btn-filled btn--primary">
+						Get started
 					</button>
 				</div>
 			</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
 		fontFamily: {
 			sans: ["Inter", "Roboto", "ui-sans-serif", "system-ui", "sans-serif"],
 		},
-		extend: {},
+		extend: {
+			colors: {
+				primary: colors.indigo,
+				secondary: colors.pink,
+			},
+		},
+		borderWidth: {
+			DEFAULT: "1px",
+			0: "0",
+			2: "2px",
+			3: "3px",
+			4: "4px",
+			6: "6px",
+			8: "8px",
+		},
 		container: {
 			center: true,
 			padding: {

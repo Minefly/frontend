b4c9103f6d0544e4dfcd637e9576e8c524f3cad3
Lukas <69743171+quick007YT@users.noreply.github.com>
2021-03-16 21:48:39 -0700

    apparently yarn doesn't like "style="???

--- pages/rules.tsx
+++ pages/rules.tsx
@@ -8,7 +8,7 @@ const Rules = () => {
 				Rules | Minefly
 			</title>
 			<NavBar />
-			<div class="py-20" style="background: linear-gradient(90deg, #667eea 0%, #764ba2 100%)">
+			<div class="py-20 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
 				<div class="container mx-auto content-center">
 					<h1 class="text-4xl font-bold center text-white">
 						Rules

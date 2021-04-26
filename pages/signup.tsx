068b1fec444572b2d14f5e62ef8308085438e9d0
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-04-26 12:14:55 +0200

    Starting to work on the signup form function

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,8 +2,15 @@ import Head from "next/head";
 import Footer from "../components/footer";
 import NavBar from "../components/navbar";
 import Link from "next/link";
+import { FormEvent, useRef } from "react";
 
 const signup = () => {
+    const email = useRef<HTMLInputElement>(null);
+    const password = useRef<HTMLInputElement>(null);
+    function handleSubmit(event: FormEvent) {
+        
+    }
+
     return (
         <>
             <Head>
@@ -13,7 +20,7 @@ const signup = () => {
             <div className="relative flex flex-grow justify-center items-center bg-gray-800">
                 <div className="bg-gray-900 rounded shadow-2xl p-2">
                     <h2 className="text-lg font-semibold text-center">Sign up</h2>
-                    <form className="mx-6 my-3 flex flex-col">
+                    <form className="mx-6 my-3 flex flex-col" onSubmit={handleSubmit}>
                         <label 
                             htmlFor="email" 
                             className="text-gray-300 text-sm pl-2"
@@ -21,7 +28,8 @@ const signup = () => {
                         <input 
                             type="email" 
                             id="email" 
-                            name="fname" 
+                            name="fname"
+                            ref={email}
                             className="bg-gray-850 focus:outline-none rounded shadow px-2 py-1 w-80 " 
                             required
                         />
@@ -32,12 +40,12 @@ const signup = () => {
                         >Password</label>
                         <input 
                             type="password" 
-                            id="pass" 
+                            id="pass"
+                            ref={password}
                             name="lname" 
                             className="bg-gray-850 focus:outline-none rounded shadow px-2 py-1 w-80 " 
                             required
                         />
-
                         <input type="submit" value="Sign up" className="btn btn-filled btn--primary mt-3" />
                     </form>
                     

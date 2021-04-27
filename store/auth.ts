413bd6aac7ec87bca9b5342a9379ddf54f53291f
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-04-27 15:41:21 +0200

    Created auth store (should be used for auth-related data)

---
+++ store/auth.ts
@@ -0,0 +1,24 @@
+import create from "zustand";
+import { persist } from "zustand/middleware";
+
+type AuthState = {
+  loggedIn: boolean;
+  setLoggedIn: (loggedIn: boolean) => void;
+  expiresAt?: Date;
+  userId?: string;
+  userType?: "user" | "admin";
+};
+
+export const useAuthStore = create<AuthState>(
+  persist(
+    (set) => ({
+      loggedIn: false,
+      setLoggedIn: (loggedIn: boolean) => set({ loggedIn }),
+    }),
+    {
+      name: "auth-storage",
+    }
+  )
+);
+
+useAuthStore((state) => state);

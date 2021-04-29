445046c4831d3ca0471fcd8fd8622f9877f1b4e9
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-04-29 09:34:56 -0700

    test using heroicons bc I can't build locally for some reason

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { Dispatch, FC, SetStateAction, useState } from "react";
 import Link from "next/link";
 import { useAuthStore } from "../store/auth";
+import { HomeIcon } from "@heroicons/react/outline"
 
 interface SidebarProps {
     sidebarOpen?: [boolean, Dispatch<SetStateAction<boolean>>];
@@ -91,20 +92,7 @@ const Sidebar: SidebarComponent = (props) => {
                                                     "bg-gray-100 dark:bg-gray-800")
                                             }
                                         >
-                                            <svg
-                                                className="h-6 w-auto mr-2"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                stroke="currentColor"
-                                            >
-                                                <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    stroke-width="2"
-                                                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                                                />
-                                            </svg>
+                                            <HomeIcon /> 
                                             Home
                                         </a>
                                     </Link>

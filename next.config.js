e5a1818c2f8888f30fa19e5bcf1209954c640537
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-09-03 11:35:01 +0200

    Implemented mdx support

--- next.config.js
+++ next.config.js
@@ -1,22 +1,30 @@
 /** @type {import('next').NextConfig} */
 import withPWA from "next-pwa";
-module.exports = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: "out",
-    disable: process.env.NODE_ENV === "development",
-    register: true,
-  },
-  future: {
-    webpack5: true,
-  },
-  pageExtensions: ["tsx", "mdx"],
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+import mdx from "@next/mdx";
 
-    return config;
-  },
+const withMdx = mdx({
+  extension: /\.mdx?$/,
 });
+
+module.exports = withPWA(
+  withMdx({
+    reactStrictMode: true,
+    pwa: {
+      dest: "out",
+      disable: process.env.NODE_ENV === "development",
+      register: true,
+    },
+    future: {
+      webpack5: true,
+    },
+    pageExtensions: ["tsx", "mdx", "md"],
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+
+      return config;
+    },
+  })
+);
